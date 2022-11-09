d09e0ca779ef924f909e16c6030df4995d04194a
Worldcompute <xmik2022@gmail.com>
2022-11-09 20:32:52 +0300

    1

--- send.php
+++ send.php
@@ -4,9 +4,19 @@ $username = "username";
 $password = "password";
 
 // Create connection
-mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
 $conn = new mysqli($servername, $username, $password);
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
 
 // Create database
 $sql = "CREATE DATABASE myDB";
-$conn->query($sql);
+if ($conn->query($sql) === TRUE) {
+  echo "Database created successfully";
+} else {
+  echo "Error creating database: " . $conn->error;
+}
+
+$conn->close();
+?>
